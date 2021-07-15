f179faaa22b538e2f33b3a0cbda524df0a710f02
Cristhian <cristhian.torres@mail.escuelaing.edu.co>
2021-07-15 10:12:14 -0500

    version 1.0.0

---
+++ src/main/java/com/example/testingweb/WebLayerTest.java
@@ -0,0 +1,24 @@
+package com.example.testingweb;
+
+
+import io.lettuce.core.internal.LettuceStrings;
+import org.apache.tomcat.util.descriptor.web.FragmentJarScannerCallback;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultHandler;
+
+@WebMvcTest
+public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
